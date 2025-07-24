79a9135918f7f9238e30c75bc9f9de464d591ec7
shri31negi <144342439+shri31negi@users.noreply.github.com>
2025-07-24 07:22:00 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import numpy as np
+import pickle
+
+model = pickle.load(open('customer_churn.pkl', 'rb'))
+
+st.title("Customer Churn Prediction")
+
+gender = st.selectbox("Gender", ['Male', 'Female'])
+tenure = st.slider("Tenure (months)", 0, 72)
+monthly = st.number_input("Monthly Charges")
+total = st.number_input("Total Charges")
+
+gender = 1 if gender == 'Male' else 0
+
+input_data = np.array([[gender, tenure, monthly, total]])  
+prediction = model.predict(input_data)
+
+if prediction[0] == 1:
+    st.warning("🟡 This customer is likely to churn.")
+else:
+    st.success("🟢 This customer is likely to stay.")
